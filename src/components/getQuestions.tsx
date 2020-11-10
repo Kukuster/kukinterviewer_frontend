a719a1cd70d7146159eff617d03309193c69bb51
Kukuster <KukusterMOP@gmail.com>
2020-11-11 00:04:52 +0200

    create-react-app w typescript

---
+++ src/components/getQuestions.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class getQuestions extends Component {
+    render() {
+        return (
+            <div className="questions-list">
+                
+            </div>
+        )
+    }
+}
